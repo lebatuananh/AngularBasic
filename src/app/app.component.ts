cb4ca35e865a39f6f71dc1e517bbdc35cb833d1d
Le Ba Tuan Anh <tuananh300496@gmail.com>
2018-08-25 22:42:39 +0700

    Form BS4 With Angular Basic

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -6,6 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  public power = 5;
-  public factor = 1;
+  onSubmit(value: any) {
+    console.log(value);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,11 +2,10 @@ import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
 import { FormsModule } from "@angular/forms";
 import { AppComponent } from "./app.component";
-import { PipeComponent } from "./pipe/pipe.component";
-import { ExponentialStrengthPipe } from "./exponential-strength.pipe";
+
 
 @NgModule({
-  declarations: [AppComponent, PipeComponent, ExponentialStrengthPipe],
+  declarations: [AppComponent],
   imports: [BrowserModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent]
